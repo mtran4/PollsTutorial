a12ff0f96913857e01d82c3a4ed6566745d2ca90
Maggie <77594458+mtran4@users.noreply.github.com>
2021-02-19 14:08:40 -0500

    Deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-# Test comment
-# Test comment 2
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
